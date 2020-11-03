1d57c049782cdb137322e552fbad2f619c45b707
Fernando Andrade <fandrade@afsgroup.nl>
2020-11-03 08:01:38 +0100

    :truck: this was a mistake ove server away xD

--- srv/README.md
+++
@@ -1,5 +0,0 @@
-
-
-## for the database
-
-docker run --name reactive-sql -p 3306:3306 -e MYSQL_DATABASE=reactive -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql --default-authentication-plugin=mysql_native_password
--- srv/src/Controllers/MainController.ts
+++
@@ -1,9 +0,0 @@
-import {Request, Response } from 'express';
-import sendmail from '../Infrastructure/mailer';
-
-export default {
-  async index(request: Request, response: Response) {
-    sendmail();
-    return response.status(200).json({"cenas": "claras"});
-  }
-}
