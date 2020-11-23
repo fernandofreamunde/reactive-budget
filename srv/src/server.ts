eeaef0533f64d4acd622d6534e235384de29d87e
Fernando Andrade <fernandofreamunde@gmail.com>
2020-11-23 10:49:59 +0100

    🔒 CORS set them up

--- srv/src/server.ts
+++ srv/src/server.ts
@@ -5,10 +5,16 @@ import 'express-async-errors';
 import './Infrastructure/DatabaseConnection';
 import accessMiddleware from './Middleware/AccessMiddleware';
 
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+  exposedHeaders: 'new-token' 
+}
 const app = express();
+//chekc cors documentation https://github.com/expressjs/cors
+app.use(cors(corsOptions));
 app.use(accessMiddleware);
 app.use(express.json());
-app.use(cors(/* {origin ... }*/));
 app.use(routes);
 
 app.listen(3333);
